6271f3e5ab9363980f7c0a34f43c3cabda4618bf
yukie-ai <68629886+yukie-ai@users.noreply.github.com>
2020-10-30 16:18:07 +0900

    更新

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -7,8 +7,27 @@ $(function(){
   })
   
 
+  $("#button01").on("click",function(){
+    let students_txt = "";
 
+    for(let i=0; i<students.length; i++){
+      students_txt += students[i]
+      students_txt += '/'
+    }
 
+    $("#content").text(students_txt);
+  })
 
+  $("#button02").on("click",function(){
+    let students_txt = "";
+
+    for(let i=0; i<students.length; i++){
+      students_txt += "<div id='student" + "' class='student'>" + students[i] + 
+      "</div>"
+    }
+
+    $("#content").html(students_txt);
+  })
+  
 
 })
--- sample/js/script03.js
+++ sample/js/script03.js
@@ -12,6 +12,32 @@ $(function(){
     console.log(students)
   })
 
+  $("#button01").on("click",function(){
+    $("#content").text(students[0].name + ':' + students[0].sport)
+  })
+
+  $("#button02").on("click",function(){
+    let students_txt = "";
+
+    for(let i=0; i<students.length; i++){
+      students_txt += students[i].name + 'i' + students[i].sport + '/'
+    }
+
+    $("#content").text(students_txt);
+  })
+
+  $("#button03").on("click",function(){
+    let students_txt = "<div class='students'>";
+
+    for(let i=0; i<students.length; i++){
+      students_txt += `<div id='student${i}' class='student'><p>${students[i].name}</
+      p><p>${students[i].sport}</p><p>${students[i].age}</p></div>`
+    }
+
+    students_txt += "</div>";
+
+    $("#content").html(students_txt);
+  })
 
 
 
